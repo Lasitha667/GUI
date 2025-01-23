24d4a5cc06a912cb20b59acc6f678c48fc4d0b3b
Lasitha667 <klasitha2089@gmail.com>
2025-01-23 17:02:10 +0530

    name update of cloths

--- OnlineClothing/src/Components/Cards/cardsmen/cardsmen.jsx
+++ OnlineClothing/src/Components/Cards/cardsmen/cardsmen.jsx
@@ -2,55 +2,55 @@ import React, { useState } from "react";
 import "./cardsmen.css";
 
 const products = [
-  {
-    image: "card1.jpg",
-    title: "Textured Crop Top",
-    price: 1800,
-    installment: "3 X Rs. 600.00 with mintpay",
-  },
-  {
-    image: "card2.jpg",
-    title: "Unisex Drop Shoulder Shirt",
-    price: 2600,
-    installment: "3 X Rs. 866.67 with mintpay",
-  },
-  {
-    image: "card3.jpg",
-    title: "Outline Ribbed Top",
-    price: 1400,
-    installment: "3 X Rs. 466.67 with mintpay",
-  },
-  {
-    image: "card5.jpg",
-    title: "Ladies Workwear Pant",
-    price: 2500,
-    installment: "3 X Rs. 833.33 with mintpay",
-  },
-  {
-    image: "card4.jpg",
-    title: "Ladies Workwear Pant",
-    price: 2500,
-    installment: "3 X Rs. 833.33 with mintpay",
-  },
-  {
-    image: "image1.jpg",
-    title: "Ladies Workwear Pant",
-    price: 2500,
-    installment: "3 X Rs. 833.33 with mintpay",
-  },
-   {
-    image: "card7.jpg",
-    title: "Ladies Workwear Pant",
-    price: 2500,
-    installment: "3 X Rs. 833.33 with mintpay",
-  },
-  {
-    image: "card8.jpg",
-    title: "Outline Ribbed Top",
-    price: 1400,
-    installment: "3 X Rs. 466.67 with mintpay",
-  },
-];
+    {
+      image: "card1.jpg",
+      title: "Ladies  Top",
+      price: 1800,
+      installment: "3 X Rs. 600.00 with mintpay",
+    },
+    {
+      image: "card2.jpg",
+      title: "Ladies Top",
+      price: 2600,
+      installment: "3 X Rs. 866.67 with mintpay",
+    },
+    {
+      image: "card3.jpg",
+      title: "Mens Shirt",
+      price: 1400,
+      installment: "3 X Rs. 466.67 with mintpay",
+    },
+    {
+      image: "card5.jpg",
+      title: "Mens T-shirt",
+      price: 2500,
+      installment: "3 X Rs. 833.33 with mintpay",
+    },
+    {
+      image: "card4.jpg",
+      title: "Ladies Top",
+      price: 2500,
+      installment: "3 X Rs. 833.33 with mintpay",
+    },
+    {
+      image: "image1.jpg",
+      title: "Ladies Shirt",
+      price: 2500,
+      installment: "3 X Rs. 833.33 with mintpay",
+    },
+     {
+      image: "card7.jpg",
+      title: "Mens Short",
+      price: 2500,
+      installment: "3 X Rs. 833.33 with mintpay",
+    },
+    {
+      image: "card8.jpg",
+      title: "Mens Shirt",
+      price: 1400,
+      installment: "3 X Rs. 466.67 with mintpay",
+    },
+  ];
 
 const ProductDisplay2 = () => {
   const [cart, setCart] = useState([]);
