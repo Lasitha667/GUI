6fc1200072e4f1951416904a58af397e3b98e154
Lasitha667 <klasitha2089@gmail.com>
2025-03-02 21:43:29 +0530

    update to the image slider

--- OnlineClothing/src/Components/Pages/Home.jsx
+++ OnlineClothing/src/Components/Pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 
-import Footer from '../Footer/Footer.jsx';
+
 import Banner from '../Banner/Banner.jsx';
-import Moving from '../Moving Bar/Moving.jsx';
+import Moving from '../MovingUpper/MovingUpper.jsx';
 import Cards from '../Cards/Cards.jsx';
 import Slider from '../ImageSlider/ImageSlider.jsx';
 import Features from '../BottomInfo/bottominfo.jsx';
@@ -17,7 +17,7 @@ function Home(){
            
           <Slider/>
           <Moving/>
-          <DeliveryBar/>
+          
           <Banner/>
           <Cards/>
           <Features/>
